7c915e1a6248294c2735a27a948026dca7a6bf8c
MrAllin1 <alidemandi@gmail.com>
2024-04-17 22:57:54 +0200

    all products replaced with stored procedures

--- src/features/products/data/get_all_products.ts
+++ src/features/products/data/get_all_products.ts
@@ -4,25 +4,7 @@ import connection from '../../../database';
 const getAllProducts = () => {
     return new Promise((resolve, reject) => {
         connection.query(`
-        SELECT 
-        P.ProductID,
-        P.Name AS ProductName,
-        C.Name AS CategoryName,
-        B.Name AS BrandName,
-        M.Name AS MaterialName,
-        P.Price,
-        P.StockQuantity,
-        P.Weight,
-        P.Image,
-        P.Rating
-    FROM 
-        Produkti P
-    left JOIN 
-        Kategoria C ON P.TypeID = C.CategoryID
-    left JOIN 
-        Brendi B ON P.BrandID = B.BrandID
-    left JOIN 
-        Materiali M ON P.MaterialID = M.MaterialID;
+        call get_all_products();
     `
             , (error: string, results: string) => {
                 if (error) {
