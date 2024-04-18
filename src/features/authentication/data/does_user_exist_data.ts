c708f40410c19dfff6a0029b7ccc9a24bd7e8db7
MrAllin1 <alidemandi@gmail.com>
2024-04-18 13:30:36 +0200

    Refactor SQL queries to use stored procedures

--- src/features/authentication/data/does_user_exist_data.ts
+++ src/features/authentication/data/does_user_exist_data.ts
@@ -4,11 +4,19 @@ import connection from '../../../database';
 
 const getDoesUserExistData = (email: string) => {
   return new Promise((resolve, reject) => {
-    connection.query(`call check_if_user_exists(${email})`,
+    connection.query(`call check_if_user_exists('${email}')`,
       async (error: string, results: string) => {
         if (error) {
           reject(error);
         }
+
+        console.log(email, 'email');
+        console.log(results, 'results');
+        
+        if (results.length === 0 || results[0].length === 0) {
+          resolve(false); // Return false if results are empty
+        }
+
         resolve(results);
       });
   });
--- src/features/authentication/data/insert_new_user.ts
+++ src/features/authentication/data/insert_new_user.ts
@@ -2,13 +2,15 @@ import connection from '../../../database';
 
 const insertNewUser = (username: string, email: string, hashedPassword: string) => {
   return new Promise((resolve, reject) => {
-    connection.query(`call register_user(${username},${email},${hashedPassword})`,
-    async (error: any, results: any,) => {
-      if (error) {
-        reject(false); // Reject with false if there's an error
-      }
-      resolve(true); // Resolve with true if insertion is successful
-    })
+    connection.query(`call register_user('${username}','${email}','${hashedPassword}')`,
+      async (error: any, results: any,) => {
+        if (error) {
+          reject(error); // Reject with the error object
+        } else {
+          resolve(true); // Resolve with true if insertion is successful
+        }
+      })
   });
 };
+
 export default insertNewUser;
