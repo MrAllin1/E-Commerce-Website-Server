862903b31ef5b30687de0c08cf73151c5c91b1e6
MrAllin1 <alidemandi@gmail.com>
2024-04-22 00:12:55 +0200

    Add admin authentication to the upload endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import allOffersRouter from './features/offers/routes/all_offers';
 import allProductsRouter from './features/products/routes/all_products';
 import insertProductRouter from './features/products/routes/insert_product';
 import uploadImage from './features/uploadImages/upload_image';
+import admin_auth from './middleware/check_admin_auth';
 const cors = require('cors');
 
 const app = express();
@@ -20,7 +21,7 @@ app.use('/logIn', logInRouter);
 app.use('/offers', allOffersRouter);
 app.use('/products', allProductsRouter);
 app.use('/insertProduct', insertProductRouter);
-app.use('/upload', uploadImage);
+app.use('/upload', admin_auth, uploadImage);
 
 app.listen(port, () => {
     console.log(`Server is running on url http://localhost:${port}`);
