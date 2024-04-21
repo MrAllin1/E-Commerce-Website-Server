965892496c2ec99c7d4bc8b12d6b34ea046711ca
MrAllin1 <alidemandi@gmail.com>
2024-04-21 17:38:59 +0200

    small fix on insert product endpoint

--- src/features/products/data/insert_product.ts
+++ src/features/products/data/insert_product.ts
@@ -3,11 +3,11 @@ import connection from '../../../database';
 import { InsertProductRequest } from '../../../types/insert_product';
 
 const insertProduct = (body: InsertProductRequest): Promise<boolean> => {
-    const { productName, productPrice, StockQuantity, Weight, Image, Rating, CategoryName, BrandName, MaterialName } = body;
+    const { productName, productPrice, StockQuantity, Weight, Rating, CategoryName, BrandName, MaterialName } = body;
 
     return new Promise((resolve, reject) => {
-        connection.query(`CALL insert_product(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-            [productName, productPrice, StockQuantity, Weight, Image, Rating, CategoryName, BrandName, MaterialName], async (error: any, results: any,) => {
+        connection.query(`CALL insert_product(?, ?, ?, ?, ?, ?, ?, ?)`,
+            [productName, productPrice, StockQuantity, Weight, Rating, CategoryName, BrandName, MaterialName], async (error: any, results: any,) => {
                 if (error) {
                     reject(false); // Reject with false if there's an error
                 }
