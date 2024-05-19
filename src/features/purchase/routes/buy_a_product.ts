fe6b4ee1213f38edf8fd6dd5e867665fb42b4a23
MrAllin1 <alidemandi@gmail.com>
2024-05-19 22:48:15 +0200

    feat: Added route for purchasing a product

---
+++ src/features/purchase/routes/buy_a_product.ts
@@ -0,0 +1,19 @@
+import express, { Router, Request, Response } from 'express';
+import postBuyProduct from '../data/post_buy_product';
+import { buyProductRequest, buyProductResponse } from '../../../types/buy_product';
+
+const router: Router = express.Router();
+
+router.post('/', async (req: Request<buyProductRequest>, res: Response<buyProductResponse>) => {
+    const { userId, productId, quantity, firstName, lastName, phoneNumber, email, address, city } = req.body as buyProductRequest;
+    try {
+        await postBuyProduct(userId, productId, quantity, firstName, lastName, phoneNumber, email, address, city);
+        res.json({ success: true });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false });
+
+    }
+});
+
+export default router;
