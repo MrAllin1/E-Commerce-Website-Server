9f61376ec07f44fda5bf4dd1ff43470ea589678d
MrAllin1 <alidemandi@gmail.com>
2024-07-13 14:42:27 +0200

    chore: Remove unnecessary console.log statements and comments

--- src/features/purchase/routes/user_buy_history.ts
+++ src/features/purchase/routes/user_buy_history.ts
@@ -4,10 +4,8 @@ import getSpecificUserBuyHistory from '../data/get_specific_user_buy_history';
 const router: Router = express.Router();
 
 router.get('/:userId', async (req, res) => {
-    console.log('Request:', req.params);
     const userId: string = req.params.userId;
     const products: any = await getSpecificUserBuyHistory(userId);
-    console.log('Products:', products);
     res.json(products);
 });
 export default router;
